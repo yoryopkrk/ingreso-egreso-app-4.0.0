bf53cb1a47ddc371c6bd618d71851115d8d27d94
yoryopkrk <yoryo.puncrist@gmail.com>
2026-07-08 17:15:43 -0400

    se agrega script start:dev. se corrigen @if y @for para que el proyecto use angular actual. se quitan los style directo en html y se crean clases para estos. se corrige el grafico de estadisticas

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,33 +1,42 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { Store } from "@ngrx/store";
-import { Subscription } from "rxjs";
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import type { ChartConfiguration, ChartType } from 'chart.js';
 
-import { IngresoEgreso } from "../ingreso-egreso.model";
-import * as fromIngresoEgreso from "../ingreso-egreso.reducer";
-import { selectIngresoEgresoItems } from "../ingreso-egreso.selectors";
+import { IngresoEgreso } from '../ingreso-egreso.model';
+import * as fromIngresoEgreso from '../ingreso-egreso.reducer';
+import { selectIngresoEgresoItems } from '../ingreso-egreso.selectors';
 
 @Component({
-  selector: "app-estadistica",
+  selector: 'app-estadistica',
   standalone: false,
-  templateUrl: "./estadistica.component.html",
+  templateUrl: './estadistica.component.html',
   styles: [],
 })
-export class EstadisticaComponent implements OnInit {
-  ingresos: number;
-  egresos: number;
+export class EstadisticaComponent implements OnInit, OnDestroy {
+  ingresos = 0;
+  egresos = 0;
 
-  cuantosIngresos: number;
-  cuantosEgresos: number;
+  cuantosIngresos = 0;
+  cuantosEgresos = 0;
 
   subscription: Subscription = new Subscription();
 
-  public doughnutChartLabels: string[] = ["Ingresos", "Egresos"];
-  public doughnutChartData: number[] = [];
+  public doughnutChartType: ChartType = 'doughnut';
 
-  constructor(private store: Store<fromIngresoEgreso.AppState>) {}
+  public doughnutChartData: ChartConfiguration<'doughnut'>['data'] = {
+    labels: ['Ingresos', 'Egresos'],
+    datasets: [
+      {
+        data: [0, 0],
+      },
+    ],
+  };
 
-  ngOnInit() {
+  constructor(private readonly store: Store<fromIngresoEgreso.AppState>) {}
+
+  ngOnInit(): void {
     this.subscription = this.store
       .select(selectIngresoEgresoItems)
       .subscribe((items) => {
@@ -35,7 +44,11 @@ export class EstadisticaComponent implements OnInit {
       });
   }
 
-  contarIngresoEgreso(items: IngresoEgreso[]) {
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  contarIngresoEgreso(items: IngresoEgreso[]): void {
     this.ingresos = 0;
     this.egresos = 0;
 
@@ -43,15 +56,23 @@ export class EstadisticaComponent implements OnInit {
     this.cuantosIngresos = 0;
 
     items.forEach((item) => {
-      if (item.tipo === "ingreso") {
+      if (item.tipo === 'ingreso') {
         this.cuantosIngresos++;
-        this.ingresos += item.monto;
-      } else {
-        this.cuantosEgresos++;
-        this.egresos += item.monto;
+        this.ingresos += Number(item.monto);
+        return;
       }
+
+      this.cuantosEgresos++;
+      this.egresos += Number(item.monto);
     });
 
-    this.doughnutChartData = [this.ingresos, this.egresos];
+    this.doughnutChartData = {
+      labels: ['Ingresos', 'Egresos'],
+      datasets: [
+        {
+          data: [this.ingresos, this.egresos],
+        },
+      ],
+    };
   }
 }
